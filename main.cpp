a234c03ef8539cb8a0264ded37c1a139ba362a55
Yunus Emre Yiğit <yigityunus442@gmail.com>
2021-12-16 00:02:00 +0300

    classes formed

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Vehicles{
+    public:
+        int id;
+        string location;
+        float distance;
+        int speed;
+}; 
+
+class Request{
+    public:
+        string location;
+        float distance;
+        int lucky_num;
+};
+
